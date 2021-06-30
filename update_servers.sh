c67a51d9788a53abad7cb78e3f35d8c1a12a0f26
duncte123 <contact@duncte123.me>
2021-06-30 11:24:50 +0200

    [ci skip]  add a simple script to update all nodes

---
+++ update_servers.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SERVERS="${ll_fr1_ip} ${ll_ca1_ip} ${ll_fr2_ip} ${ll_ca2_ip}"
+
+# load all monitors
+for SERVER in ${SERVERS}; do
+  gnome-terminal -- /usr/bin/fish -c "ssh -p ${SERVER_PORT} root@${SERVER} 'journalctl -fu lavalink -o cat'"
+done
+
+
+for SERVER in ${SERVERS}; do
+  echo "Now updating ${SERVER}"
+
+  # stop lavalink
+  echo "stopping node"
+  ssh -p $SERVER_PORT root@$SERVER 'sudo systemctl stop lavalink'
+  echo "stopped node"
+  # upload file
+  scp -P $SERVER_PORT ./LavalinkServer/build/libs/Lavalink.jar root@$SERVER:./lavalink/
+  # start lavalink
+  echo "sarting node"
+  ssh -p $SERVER_PORT root@$SERVER 'sudo systemctl start lavalink'
+  echo "started node"
+
+  # wait a bit
+  sleep 30s
+done
+
+echo "Done"
