0d727320f2621ddcb2c9414dc44206ed2483452f
duncte123 <contact@duncte123.me>
2021-06-30 15:44:00 +0200

    [ci skip] change env var (thanks laravel)

--- update_servers.sh
+++ update_servers.sh
@@ -4,7 +4,7 @@ SERVERS="${ll_fr1_ip} ${ll_ca1_ip} ${ll_fr2_ip} ${ll_ca2_ip}"
 
 # load all monitors
 for SERVER in ${SERVERS}; do
-  gnome-terminal -- /usr/bin/fish -c "ssh -p ${SERVER_PORT} root@${SERVER} 'journalctl -fu lavalink -o cat'"
+  gnome-terminal -- /usr/bin/fish -c "ssh -p ${VPS_PORT} root@${SERVER} 'journalctl -fu lavalink -o cat'"
 done
 
 
@@ -13,13 +13,13 @@ for SERVER in ${SERVERS}; do
 
   # stop lavalink
   echo "stopping node"
-  ssh -p $SERVER_PORT root@$SERVER 'sudo systemctl stop lavalink'
+  ssh -p $VPS_PORT root@$SERVER 'sudo systemctl stop lavalink'
   echo "stopped node"
   # upload file
-  scp -P $SERVER_PORT ./LavalinkServer/build/libs/Lavalink.jar root@$SERVER:./lavalink/
+  scp -P $VPS_PORT ./LavalinkServer/build/libs/Lavalink.jar root@$SERVER:./lavalink/
   # start lavalink
   echo "sarting node"
-  ssh -p $SERVER_PORT root@$SERVER 'sudo systemctl start lavalink'
+  ssh -p $VPS_PORT root@$SERVER 'sudo systemctl start lavalink'
   echo "started node"
 
   # wait a bit
